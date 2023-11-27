74db1c7fd009a3b1801021fae88cc2d9e2c5c3e6
reyda <reyda@LAPTOP-ALH488S6>
2023-11-27 00:24:23 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Practica_18
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            button2 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Stencil", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(75, 321);
+            button1.Name = "button1";
+            button1.Size = new Size(135, 37);
+            button1.TabIndex = 0;
+            button1.Text = "Desición";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Rockwell", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(801, 22);
+            label1.TabIndex = 1;
+            label1.Text = "Diagrama que pida 3 lados de un triangulo y diga si es equilatero, isoseles o escaleno.";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(465, 73);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(339, 343);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Stencil", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(242, 321);
+            button2.Name = "button2";
+            button2.Size = new Size(135, 37);
+            button2.TabIndex = 3;
+            button2.Text = "Borrar";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Rockwell", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(12, 84);
+            label2.Name = "label2";
+            label2.Size = new Size(162, 44);
+            label2.TabIndex = 4;
+            label2.Text = "Longitud lado A:\r\n\r\n";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Rockwell", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(12, 158);
+            label3.Name = "label3";
+            label3.Size = new Size(160, 44);
+            label3.TabIndex = 5;
+            label3.Text = "Longitud lado B:\r\n\r\n";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Rockwell", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(12, 233);
+            label4.Name = "label4";
+            label4.Size = new Size(163, 44);
+            label4.TabIndex = 6;
+            label4.Text = "Longitud lado C:\r\n\r\n";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Rockwell", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(12, 397);
+            label5.Name = "label5";
+            label5.Size = new Size(179, 44);
+            label5.TabIndex = 7;
+            label5.Text = "Tipo de triangulo: \r\n\r\n";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(180, 79);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(230, 31);
+            textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(178, 153);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(230, 31);
+            textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(178, 228);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(230, 31);
+            textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(180, 392);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(230, 31);
+            textBox4.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(816, 450);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Triangulos";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Button button2;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+    }
+}
